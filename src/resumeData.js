e232dc4501afe5bfb9b1d249b538a0e5432f4f4a
xprtcenter <81644623+xprtcenter@users.noreply.github.com>
2023-11-15 10:08:18 +0530

    updated code

---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+	imagebaseurl: "https://xprtcenter.github.io/",
+	name: "Ritesh Rastogi",
+	role: "Frontend Developer and Data Scientist",
+	linkedinId: "https://www.linkedin.com/in/sabsealaghume/",
+	skypeid: "Your skypeid",
+	roleDescription:
+		"I like dabbling in various parts of frontend development and like to learn about new technologies, make technical devices or simply play flute in my free time.",
+	socialLinks: [
+		{
+			name: "linkedin",
+			url: "https://www.linkedin.com/in/sabsealaghume/",
+			className: "fa fa-linkedin",
+		},
+		{
+			name: "github",
+			url: "http://github.com/xprtcenter",
+			className: "fa fa-github",
+		},
+		{
+			name: "skype",
+			url: "http://twitter.com/rbhatia46",
+			className: "fa fa-twitter",
+		},
+	],
+	aboutme:
+		"I am currently a technical support engineer at Siddh Hospital and managing hole IT department as In-Charge position. I am a self taught Full Stack Web Developer, currently diving deeper into UI/UX designer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+	address: "India",
+	website: "https://xprtcenter.github.io",
+	education: [
+		{
+			UniversityName: "Shobhit Institute of Engineering & Technology, Meerut",
+			specialization: "Post Graduation",
+			MonthOfPassing: "Aug",
+			YearOfPassing: "2018",
+			Achievements: "Some Achievements",
+		},
+		/* 	{
+			UniversityName: "Some University",
+			specialization: "Some specialization",
+			MonthOfPassing: "Jan",
+			YearOfPassing: "2018",
+			Achievements: "Some Achievements",
+		}, */
+	],
+	work: [
+		{
+			CompanyName: "Some Company",
+			specialization: "Some specialization",
+			MonthOfLeaving: "Jan",
+			YearOfLeaving: "2018",
+			Achievements: "Some Achievements",
+		},
+		{
+			CompanyName: "Some Company",
+			specialization: "Some specialization",
+			MonthOfLeaving: "Jan",
+			YearOfLeaving: "2018",
+			Achievements: "Some Achievements",
+		},
+	],
+	skillsDescription: "Your skills here",
+	skills: [
+		{
+			skillname: "HTML5",
+		},
+		{
+			skillname: "CSS",
+		},
+		{
+			skillname: "Reactjs",
+		},
+	],
+	portfolio: [
+		{
+			name: "project1",
+			description: "mobileapp",
+			imgurl: "images/portfolio/phone.jpg",
+		},
+		{
+			name: "project2",
+			description: "mobileapp",
+			imgurl: "images/portfolio/project.jpg",
+		},
+		{
+			name: "project3",
+			description: "mobileapp",
+			imgurl: "images/portfolio/project2.png",
+		},
+		{
+			name: "project4",
+			description: "mobileapp",
+			imgurl: "images/portfolio/phone.jpg",
+		},
+	],
+	testimonials: [
+		{
+			description: "This is a sample testimonial",
+			name: "Some technical guy",
+		},
+		{
+			description: "This is a sample testimonial",
+			name: "Some technical guy",
+		},
+	],
+};
+
+export default resumeData;
