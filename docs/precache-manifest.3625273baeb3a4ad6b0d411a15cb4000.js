42314cf9865ec3ce5f8d15ae4af35968da0e6450
xprtcenter <81644623+xprtcenter@users.noreply.github.com>
2023-11-17 09:48:32 +0530

    after build

---
+++ docs/precache-manifest.3625273baeb3a4ad6b0d411a15cb4000.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43c62c2bd974ea32c22a",
+    "url": "/static/css/main.79ef95b5.chunk.css"
+  },
+  {
+    "revision": "43c62c2bd974ea32c22a",
+    "url": "/static/js/main.43c62c2b.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c4ad39886ba893fef666a5f131d2bc63",
+    "url": "/index.html"
+  }
+];
